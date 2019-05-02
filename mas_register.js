62e469375e828f316617da2babc5a81d2e9a6eab
Unknown <CSnow040@west-mec.org>
2019-05-01 18:05:25 -0700

    Final Commit
    Finished all JavaScript as well as commenting it all out making this project ready to be graded

--- mas_register.js
+++ mas_register.js
@@ -49,5 +49,50 @@ function sessionTest() {
 
 //calculate the registration cost and to save information about the customer's choices in session storage
 function calcCart() {
-      var confName
+      sessionStorage.confName = document.forms.regForm.elements.fnBox.value + " " + document.forms.regForm.elements.lnBox.value;
+
+      sessionStorage.confGroup = document.forms.regForm.elements.groupBox.value;
+
+      sessionStorage.confMail = document.forms.regForm.elements.mailBox.value;
+
+      sessionStorage.confPhone = document.forms.regForm.elements.phoneBox.value;
+
+      sessionStorage.confBanquet = document.forms.regForm.elements.banquetBox.value;
+
+      sessionStorage.confBanquetCost = sessionStorage.confBanquet * 55;
+
+      var selectedIndex = document.getElementById("sessionBox").selectedIndex;
+
+      if (selectedIndex !== -1) {
+            sessionStorage.confSession = document.forms.regForm.elements.sessionBox[selectedIndex].text;
+
+            sessionStorage.confSessionCost = document.forms.regForm.elements.sessionBox.value;
+      } else {
+            sessionStorage.confSession = "";
+            sessionStorage.confSessionCost = 0;
+      }
+
+      if (document.getElementById("mediaCB").checked) {
+            sessionStorage.confPack = "yes";
+            sessionStorage.confPackCost = 115;
+      } else {
+            sessionStorage.confPack = "no";
+            sessionStorage.confPackCost = 0;
+      }
+
+      sessionStorage.confTotal = parseFloat(sessionStorage.confBanquetCost) + parseFloat(sessionStorage.confSessionCost) + parseFloat(sessionStorage.confPackCost);
+
+      writeSessionValues();
+}
+
+//writes the data values from session storage in to the registration summary form
+function writeSessionValues() {
+      document.getElementById("regName").textContent = sessionStorage.confName;
+      document.getElementById("regGroup").textContent = sessionStorage.confGroup;
+      document.getElementById("regEmail").textContent = sessionStorage.confMail;
+      document.getElementById("regPhone").textContent = sessionStorage.confPhone;
+      document.getElementById("regSession").textContent = sessionStorage.confSession;
+      document.getElementById("regBanquet").textContent = sessionStorage.confBanquet;
+      document.getElementById("regPack").textContent = sessionStorage.confPack;
+      document.getElementById("regTotal").textContent = "$" + sessionStorage.confTotal;
 }
